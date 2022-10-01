4ce2e6cb8ce6ad1bf3b198441462ac228e3d0131
rockcm <rockcm@etsu.edu>
2022-10-01 18:34:04 -0400

    test

---
+++ Project2-BlackJackGame/Project2/DeckDriver.cs
@@ -0,0 +1,251 @@
+﻿////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////
+//
+// Project: Project2
+// File Name: DeckDriver.cs
+// Description: Driver class that demonstrates functionality of deck, card and hand classes. 
+// Course: CSCI 1260 – Introduction to Computer Science II
+// Author: Christian Rock
+// Created:  09/21/2022
+// Copyright: Christian Rock, 2022
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////
+
+
+
+/// <summary>
+/// Implementation of Suit and Face enums with Card Class objects and Deck objects. Creates a deck object and calls methods to perform actions on the deck object.
+/// Also used the hand class to dictate how many cards should be dealt from the deck to each player.
+/// Deck constist of card objects.
+/// 
+/// </summary>
+
+using Project2;
+using System.Runtime.CompilerServices;
+
+
+
+Deck Deck = new Deck(); /// Creates a new Deck object.
+int UserInput; // input for the amount of players 
+int UserInput2; // input for the amount of cards in each players hand.
+
+/*
+/// Displays the deck object. 
+Console.WriteLine("Original Deck of Cards:");
+Console.WriteLine(Deck);
+
+/// Shuffles the deck and then displays the shuffled deck. 
+Deck.Shuffle();
+Console.WriteLine("------------");
+Console.WriteLine("Shuffled Deck of Cards:");
+Console.WriteLine(Deck);
+
+Console.WriteLine("------------");
+
+/// Creates a second deck based on the first one using the copy constructor 
+Deck Deck2 = new Deck(Deck);
+
+/// 1st hand dealt from 2nd deck.  
+Console.WriteLine("Hand 1 from copy deck:");
+Console.WriteLine(Deck2.DealAHand(7));
+
+/// 2nd hand dealt from 2nd deck.
+Console.WriteLine("------------");
+Console.WriteLine("Hand 2 from copy deck:");
+Console.WriteLine(Deck2.DealAHand(7));
+
+// just to keep code clean
+Console.WriteLine("------------");
+
+
+
+
+Deck.Shuffle(); // just to make sure deck is shuffled for upcoming game hands
+
+// do while loop to ensure cards being dealt do not exceed deck's limit.
+do
+{
+    Console.WriteLine($"\nHow many players are there? "); // prompts user for amount of players and stores that in a variable. 
+    UserInput = Convert.ToInt32(Console.ReadLine());
+
+
+    Console.WriteLine("\nHow many cards are in each Hand? "); // prompts user for amount of cards in each hand and stores that in a variable. 
+    UserInput2 = Convert.ToInt32(Console.ReadLine());
+
+    if (UserInput * UserInput2 > Deck.DeckCards.Length) // if statement for if the users hands dealt exceeds decks card limit
+    {
+        Console.WriteLine($"\nCARDS DEALT WILL EXCEED 52, PLEASE TRY AGAIN.");
+    } 
+
+} while (UserInput * UserInput2 > Deck.DeckCards.Length); // continues to loop if card amount to be dealt is not less than 52.
+
+// for loop that deals a hand according to how many players were entered and how many cards are in each hand that was entered. 
+for (int i = 0; i < UserInput; i++)
+{
+   
+    
+        Console.WriteLine($"\nHand {i + 1}"); // prints which players hand is being displyed below. 
+        Console.WriteLine(Deck.DealAHand(UserInput2)); // prints out the hand for the player
+    
+    
+ 
+}
+
+//exit message 
+Console.WriteLine("\nThank you for using my program. \nThe program has ended, press any key to exit. ");
+Console.ReadKey();
+
+
+////////////////////////////////////////////////////////
+///Created black jack game. Based off lab 2 will create a new one for project 2 when I have time. Will try to implement hand class :)
+*/
+// shuffles deck for upcoming game.
+Deck.Shuffle();
+Console.WriteLine();
+Console.WriteLine("------------");
+
+//string for dealers hand
+Console.WriteLine("Dealer's Hand");
+string dealers = ""; 
+int counter = 0;
+
+// do while loop to create 2 cards for dealers hand but only show 1.
+do
+{
+ 
+    dealers += $"\n{Deck.DealACard()}";
+
+    counter++;
+    if(counter == 1)
+    {
+        Console.WriteLine($"\nOne hidden card " +
+            $"              {dealers}");
+    }
+} while (counter < 2);
+
+
+
+// string for players hand.
+Console.WriteLine("------------");
+Console.WriteLine("Player Hand");
+string players ="";
+int counter2 = 0;
+
+//do while loop to create players hand and shows it to player.
+do
+{
+   
+    players += $"\n{Deck.DealACard()}";
+
+    counter2++;
+    
+
+} while (counter2 < 2);
+Console.WriteLine(players);
+
+
+
+string userinput4;
+string UserInput3 = "";
+
+// asks user if they want to hit after seeing thier first 2 cards.
+Console.WriteLine("\ndo you want to hit?");
+UserInput3 = Console.ReadLine();
+string msg = "";
+
+if (UserInput3 == "n" || UserInput3 == "N") // if statement for if the user does not want to hit. 
+{
+    do // loop to show the hands after the first cards are dealt and prompts user to make dealer hit till 17 or bust. Displays final hands after completion.
+    {
+        Console.WriteLine("\nHANDS AFTER PLAYER ROUND");
+        Console.WriteLine($"\nDealers Hand: \n{dealers}\n");
+        Console.WriteLine($"\nPlayers Hand:\n{players}");
+        Console.WriteLine();
+        Console.WriteLine("dealer must hit till 17. Should Dealer hit again?");
+        userinput4 = Console.ReadLine();
+        if (userinput4 == "n" || userinput4 == "N")
+        {
+            Console.WriteLine("FINAL HANDS");
+            Console.WriteLine($"Dealers Hand: \n{dealers}\n");
+            Console.WriteLine($"Players Hand:\n{players}");
+            Console.WriteLine();
+            Console.WriteLine("Your game is completed.");
+        }
+        else if (userinput4 == "Y" || userinput4 == "y")
+        {
+            dealers += $"\n{Deck.DealACard()}";
+            Console.WriteLine(dealers);
+        }
+    }while(userinput4 == "y" || userinput4 == "Y");
+}
+
+if (UserInput3 == "y" || UserInput3 == "Y") // if statement for if the user would like to "hit" aka add a card to their hand.
+{
+
+    do // do while loop to ask user if they would like to hit again after first initital hit. 
+    {
+        players += $"\n{Deck.DealACard()}";
+        Console.WriteLine($"\nPlayers Hand: \n{players}");
+
+        Console.WriteLine("\ndo you want to hit?");
+        UserInput3 = Console.ReadLine();
+        Console.ReadKey();
+        if (UserInput3 == "n" || UserInput3 == "N") // if statement for if the user does not want to hit anymore.
+        {
+            do // loop to show the hands after the first cards are dealt and prompts user to make dealer hit till 17 or bust. Displays final hands after completion.
+            {
+                Console.WriteLine("\nHANDS AFTER PLAYER ROUND");
+                Console.WriteLine($"\nDealers Hand: \n{dealers}\n");
+                Console.WriteLine($"\nPlayers Hand:\n{players}");
+                Console.WriteLine();
+                Console.WriteLine("dealer must hit till 17. Should Dealer hit again?");
+                userinput4 = Console.ReadLine();
+                if (userinput4 == "n" || userinput4 == "N")
+                {
+                    Console.WriteLine("\nFINAL HANDS");
+                    Console.WriteLine($"\nDealers Hand: \n{dealers}\n");
+                    Console.WriteLine($"\nPlayers Hand:\n{players}");
+                    Console.WriteLine();
+                    Console.WriteLine("Your game is completed.");
+                }
+                else if (userinput4 == "Y" || userinput4 == "y")
+                {
+                    dealers += $"\n{Deck.DealACard()}";
+                    Console.WriteLine($"\nDealers Hand:\n{dealers}");
+                }
+            } while (userinput4 == "Y" || userinput4 == "y");
+        }
+    }
+    while (UserInput3 == "y" || UserInput3 == "Y");
+}
+    
+
+
+
+/*
+    do
+    {
+        Console.WriteLine("HANDS AFTER FIRST ROUND");
+        Console.WriteLine($"Dealers Hand: \n{dealers}\n");
+        Console.WriteLine($"Players Hand:\n{players}");
+        Console.WriteLine();
+        Console.WriteLine("dealer must hit till 17. Should Dealer hit again?");
+        userinput4 = Console.ReadLine();
+        if (userinput4 == "n" || userinput4 == "N")
+        {
+            Console.WriteLine("FINAL HANDS");
+            Console.WriteLine($"Dealers Hand: \n{dealers}\n");
+            Console.WriteLine($"Players Hand:\n{players}");
+            Console.WriteLine();
+            Console.WriteLine("Your game is completed.");
+        }
+        else if (userinput4 == "Y" || userinput4 == "y")
+        {
+            dealers += Deck.DealACard();
+            Console.WriteLine(dealers);
+        }
+    } while (userinput4 == "n" || userinput4 == "N");
+
+}
+    */
---
+++ Project2-BlackJackGame/Project2/Hand.cs
@@ -0,0 +1,106 @@
+﻿///////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////
+//
+// Project: Project2
+// File Name: Hand.cs
+// Description: Creates a hand that is composed of a card array.  
+// Course: CSCI 1260 – Introduction to Computer Science II
+// Author: Christian Rock
+// Created: 09/21/22
+// Copyright: Christian Rock, 2022
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////
+
+using Project2;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    /// <summary>
+    /// Hand class that is composed of a card array. 
+    /// This class is used in the driver to create hands and have cards dealt into those hands.
+    /// </summary>
+    public class Hand
+    {
+        public Card[] GameHand { get; set; } // Creating a card array attribute
+        public int HandSize { get; set; } // integer attribute to dictate hand size. 
+        public int CardsInHand { get; set; } // integer attribute to keep track of how many cards have been dealt to a hand.
+
+
+        /// <summary>
+        /// Default constructor for hand class, that creates a hand from default values.  
+        /// </summary>
+        public Hand() 
+        {
+            // setting default value for the default constructor
+            HandSize = 5;
+            CardsInHand = 0;
+            GameHand = new Card[HandSize];
+            
+        }
+
+        /// <summary>
+        /// Parameterized Constructor that takes in a integer for the handsize and creates an array of that size for cards to be dealt to. 
+        /// </summary>
+        /// <param name="handSize"> the value that will be used to determine what the hand size is </param>
+        public Hand(int handSize)
+        {
+            //setting values for parameterized constructor. 
+            CardsInHand = 0;
+            HandSize = handSize;
+            GameHand = new Card[handSize];
+           
+
+        }
+
+        /// <summary>
+        /// Copy constructor for the hand class that takes in a existing hand and copys those values to a new hand. 
+        /// </summary>
+        /// <param name="existingHand">The value of a hand that has already been created and will be passed to the constructor to create a new hand of the same value.</param>
+        public Hand(Hand existingHand)
+        {
+            HandSize = existingHand.HandSize;
+            for (int i = 0; i < existingHand.GameHand.Length;) 
+            {
+                GameHand[i] = existingHand.GameHand[i];
+            }
+           
+            
+        }
+
+        /// <summary>
+        /// Method that adds a card to the game hand, this is used in DealAHand to deal a hand from the deck. 
+        /// </summary>
+        /// <param name="Card"> the value of the card that is getting added to the hand.</param>
+        public void AddCard(Card Card)
+        {
+            Card c = new Card(Card);
+            GameHand[CardsInHand] = c;
+            CardsInHand++;
+            
+           
+
+        }
+
+        /// <summary>
+        /// to string that allows for the hand to be displayed coreectly and in an aesthetically pleasing manner.
+        /// </summary>
+        /// <returns> returns all the card values in a hand</returns>
+        public override string ToString()
+        {
+            string msg = "";
+            foreach (Card c in GameHand) // prints each card in the card array to the screen. 
+            {
+                msg += $"\n{c.ToString()}";
+            }
+            return msg;
+
+        }
+    }
+}
